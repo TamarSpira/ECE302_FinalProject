5fc3316139f2d7e5849aa321fb709779124039f7
TamarSpira <ts4847@princeton.edu>
2025-12-01 18:29:37 -0500

    Not working so well but wanted to save work

--- color_detection.py
+++ color_detection.py
@@ -1,9 +1,33 @@
 import cv2
 import time
 from picamera2 import Picamera2
+from gpiozero import Servo
 import numpy as np
 
+
+# Feedback control 
+# Currently is very bad
+
+def adjustPanTilt(error_x, error_y):
+    adjust_x = 0
+    adjust_y = tiltServo.value + (error_y / -480)
+
+    if adjust_x > 1: adjust_x = 1
+    if adjust_x < -1: adjust_x = -1
+    if adjust_y > 0: adjust_y = 0
+    if adjust_y < -1: adjust_y = -1
+
+
+    panServo.value += 0
+    tiltServo.value = adjust_y
+
+    return 0
+
+
 pi = Picamera2()
+
+panServo = Servo(19, min_pulse_width=0.0005, max_pulse_width=0.0025)
+tiltServo = Servo(13, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
 try:
     config = pi.create_preview_configuration(
@@ -18,15 +42,60 @@ try:
     lower_blue = np.array([100, 150, 50])
     upper_blue = np.array([140, 255, 255])
 
+    # Height = 480
+    # Width = 640
+
+    middle_x = 320
+    middle_y = 240
+
+    # initialize to neutral postition
+    tiltServo.value = 0.5
 
     while True:
+        start_time = time.time()
+
         frame = pi.capture_array()
         frame = cv2.blur(frame, (3,3))
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        # cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+
+        best_cntr = None
+        max_area = 0
+        for cntr in contours:
+            area = cv2.contourArea(cntr)
+            if area > max_area:
+                max_area = area
+                best_cntr = cntr
+        
+        if best_cntr is not None:
+            M = cv2.moments(best_cntr)
+            cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+            cv2.circle(frame,(cx,cy),10,(0, 0, 255),-1)
+            print("Central pos: (%d, %d)" % (cx,cy))
+
+            error_x = middle_x - cx
+            error_y = middle_y - cy
+
+            print("Error: (%d, %d)" % (error_x, error_y))
+            adjustPanTilt(error_x, error_y)
+
+
+        else:
+            print("[Warning]Car lost...")                  
+             
 
         cv2.imshow("Captured frame", frame)
         cv2.imshow("Mask", mask)
+
+        # # record end time
+        # end_time = time.time()
+        # # calculate FPS
+        # seconds = end_time - start_time
+        # fps = 1.0 / seconds
+        # print("Estimated fps:{0:0.1f}".format(fps));
 
         # Press esc to exit
         if cv2.waitKey(33) == 27:
--- test_gpio.py
+++ test_gpio.py
@@ -1,25 +1,51 @@
 # Test gpio pins for rasperry pi
 
 import lgpio as LGPIO
+from gpiozero import Servo
 import time
 
-h = LGPIO.gpiochip_open(0)
-LGPIO.gpio_claim_output(h, 18)
-LGPIO.gpio_claim_output(h, 13)
-LGPIO.gpio_claim_output(h, 19)
+# h = LGPIO.gpiochip_open(0)
+# LGPIO.gpio_claim_output(h, 18)
+# LGPIO.gpio_claim_output(h, 13)
+# LGPIO.gpio_claim_output(h, 19)
 
+# Tilt up/down:
+# Min pulse makes it look flat ahead
+# Middle pulse makes it look almost straight down
+# Max pulse turns upside down (would break gun)
 
+# Pan side/side
 
+panServo = Servo(19, min_pulse_width=0.0005, max_pulse_width=0.0025)
+tiltServo = Servo(13, min_pulse_width=0.0005, max_pulse_width=0.0025)
 # LGPIO.setmode(GPIO.BCM)
 # LGPIO.setup(18, GPIO.OUT)
 
 while True:
-    LGPIO.gpio_write(h, 18, 1)
-    LGPIO.gpio_write(h, 13, 1)
+    # LGPIO.gpio_write(h, 18, 1)
+    # LGPIO.gpio_write(h, 13, 1)
 
-    time.sleep(0.5)
-    LGPIO.gpio_write(h, 13, 0)
+    # time.sleep(0.5)
+    # LGPIO.gpio_write(h, 13, 0)
 
-    time.sleep(0.5)
-    LGPIO.gpio_write(h, 18, 0)
+    # time.sleep(0.5)
+    # LGPIO.gpio_write(h, 18, 0)
+    # time.sleep(1)
+
+    print("Turning right:")
+    tiltServo.value = -1
+    panServo.value = -1
+
+    time.sleep(1)
+
+    print("Turning to middle:")
+    tiltServo.value = -0.5
+    panServo.value = 0
+
+    time.sleep(1)
+
+    print("Turning left:")
+    tiltServo.value = 0
+    panServo.value = 1
+
     time.sleep(1)
