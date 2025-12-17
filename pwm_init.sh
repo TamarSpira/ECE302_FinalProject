6b790a59e53098a4c1cb57a29b11c8f638c2ce45
Tamar Spira <ts4847@princeton.edu>
2025-12-17 14:26:20 -0500

    Removed old comments from pwm_init.sh

--- pwm_init.sh
+++ pwm_init.sh
@@ -7,8 +7,6 @@ echo "1" | sudo tee -a "$NODE/export" > /dev/null
 echo "2" | sudo tee -a "$NODE/export" > /dev/null
 echo "3" | sudo tee -a "$NODE/export" > /dev/null
 
-# Sure, the pin is set to the correct alt mode by the dtoverlay at startup...
-# But we'll do this to protect the user (me, the user is me) from themselves:
 pinctrl set 12 a0
 pinctrl set 13 a0
 pinctrl set 18 a3
